c3dbc7c0f0a7298ac4545b4d3a0b6b4b3915ebae
GaofengPan <1254066225@qq.com>
2020-11-18 09:51:27 +0800

    The Assign2 work.

--- app/Controllers/Sing.php
+++ app/Controllers/Sing.php
@@ -8,9 +8,34 @@ public function index() {
     $records = $singers->findAll();
     // get a template parser
     $parser = \Config\Services::parser(); // tell it about the substitions
-    return $parser->setData(['records' => $records])  
-// and have it render the template with those  
-        ->render('singerlist');
+    //step1
+    $table = new \CodeIgniter\View\Table(); 
+    $headings = $singers->fields;
+    $headings = array_map('ucfirst', $headings);
+    $displayHeadings = array_slice($headings, 1, 3); 
+   // $table->setHeading(array_map('ucfirst', $displayHeadings));
+    $table->setHeading($headings[1],$headings[7],'Link');
+    foreach ($records as $record) { 
+        $nameLink = anchor("sing/showme/$record->id",'Click here'); 
+        $table->addRow($record->name,'<img src="/media/image/'.$record->image.'">',$nameLink);
+       
+    }
+    $table->addRow('<p><a href="/sing">HOME</a></p>');
+    //step2
+    $template = [ 'table_open' => '<table cellpadding="2px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">',
+        'row_alt_start' => '<tr style="background-color:#dddddd">', ]; 
+    $table->setTemplate($template);
+    $fields = [           
+        'title' => 'Chinese Pop Singers',     
+        'heading' => 'Chinese Pop Singers',         
+        'footer' => 'Copyright GaofengPan'         ];
+    return $parser->setData($fields)               
+                    ->render('templates\top') .      
+            $table->generate() .          
+            $parser->setData($fields)
+                    ->render('templates\bottom');
+    
     }
 public function showme($id)
 { 
@@ -21,9 +46,34 @@ public function showme($id)
     // get a template parser
     $parser = \Config\Services::parser();
     // tell it about the substitions
-    return $parser->setData($record)
-// and have it render the template with those  
-        ->render('onesinger');
+    //step1
+    $table = new \CodeIgniter\View\Table(); 
+    $headings = $singers->fields;
+    $headings = array_map('ucfirst', $headings);
+    $table->addRow($headings[0],$record["id"]);
+    $table->addRow($headings[1],$record["name"]);
+    $table->addRow($headings[2],$record["city"]);
+    $table->addRow($headings[3],$record["height"]);
+    $table->addRow($headings[4],$record["age"]);
+    $table->addRow($headings[5],$record["birthday"]);
+    $table->addRow($headings[6],$record["food"]);
+    $table->addRow($headings[7],"<img src='/media/image/".$record['image'].'\'/>');
+    $table->addRow('<p><a href="/sing">HOME</a></p>');
+    //step2
+    $template = [ 'table_open' => '<table cellpadding="2px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">',
+        'row_alt_start' => '<tr style="background-color:#dddddd">', ]; 
+    $table->setTemplate($template);
+    $fields = [           
+        'title' => 'Chinese Pop Singers', 
+        'heading' => 'Chinese Pop Singers', 
+        'footer' => 'Copyright GaofengPan'
+        ];
+    return $parser->setData($fields)               
+                    ->render('templates\top') .      
+            $table->generate() .          
+            $parser->setData($fields)
+                    ->render('templates\bottom');
 }
   
 }
